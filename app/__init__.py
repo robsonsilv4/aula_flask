0bbcbe8102460a82bfa504435e38ea66eafe261d
robsonsilv4 <robsonsilv410@gmail.com>
2017-11-08 11:57:03 -0300

    Arquivos do projeto

---
+++ app/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config.from_object('config')
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+lm = LoginManager()
+lm.init_app(app)
+
+from app.models import tables, forms
+from app.controllers import default
